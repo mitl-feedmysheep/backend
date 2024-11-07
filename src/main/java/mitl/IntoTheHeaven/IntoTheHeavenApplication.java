32ea291b1e1c4f4e933d478b4d62c7958401ca05
martin-dsp <kcs19542001@hotmail.com>
2024-11-07 16:54:08 +0900

    feat: 기본 셋팅

---
+++ src/main/java/mitl/IntoTheHeaven/IntoTheHeavenApplication.java
@@ -0,0 +1,13 @@
+package mitl.IntoTheHeaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntoTheHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntoTheHeavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mitl/IntoTheHeaven/IntoTheHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package mitl.IntoTheHeaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntoTheHeavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
